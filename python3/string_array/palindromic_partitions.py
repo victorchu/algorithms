9d4b6b543bce67f6545aaf2963c86b1f6df69ee9
Victor Chu <victor@victor-mac.local>
2020-05-10 17:45:18 -0700

    re-org

--- python3/string_array/palindromic_partitions.py
+++ python3/string_array/palindromic_partitions.py
@@ -20,90 +20,64 @@ Ref:
 
 """
 
-import logging
-logger = logging.getLogger(__name__)
+from typing import List
 
 
-def is_palindrome(s):
-    """Check if the specified string is a palindrome"""
-    retval = True
-    i = 0
-    n = len(s)
-    for i in range(n//2):
-        if s[i] != s[n-i-1]:
-            retval = False
-            break
-    return retval
+class Solution:
+    def find_palindromic_partitions(self, s: str) -> List[List]:
+        """Find and print all palidromes in a strings.
+
+        We can print the partitions when we found them.
+        Or, we can return them as a list of lists.
+
+        """
+        def is_palindrome(s):
+            """Check if the specified string is a palindrome"""
+            retval = True
+            i = 0
+            n = len(s)
+            for i in range(n//2):
+                if s[i] != s[n-i-1]:
+                    retval = False
+                    break
+            return retval
 
 
-def find_palindromic_partitions(s):
-    """Find and print all palidromes in a strings.
+        def helper(palindrome_list, s):
+            # Use a list of lists to store all results.
+            lists = list()
+            if len(s) == 0:
+                # Reached the end of the string. Print the palindrom list.
+                lists.append(palindrome_list)
+            else:
+                for i in range(1, len(s)+1):
+                    # Iterate through all sub-strings starting from the beginning
+                    entry = s[0:i]
+                    # If it is an palindrome, use recursion to handle the rest of the string
+                    if is_palindrome(entry):
+                        new_list = palindrome_list + [entry]
+                        lists += helper(new_list, s[i:])
+            return lists
 
-    We can print the partitions when we found them.
-    Or, we can return them as a list of lists.
-
-    """
-    def helper(palindrome_list, s):
-        # Use a list of lists to store all results.
-        lists = list()
-        if len(s) == 0:
-            # Reached the end of the string. Print the palindrom list.
-            #logger.debug("FOUND {}".format(palindrome_list))
-            #lists += [palindrome_list]
-            lists.append(palindrome_list)
-        else:
-            for i in range(1, len(s)+1):
-                # Iterate through all sub-strings starting from the beginning
-                entry = s[0:i]
-                # If it is an palindrome, use recursion to handle the rest of the string
-                if is_palindrome(entry):
-                    new_list = palindrome_list + [entry]
-                    lists += helper(new_list, s[i:])
+        # Use the helper function to handle all of the work.
+        # Start with an empty list
+        lists = helper(list(), s)
         return lists
-
-    # Use the helper function to handle all of the work.
-    # Start with an empty list
-    lists = helper(list(), s)
-    for l in lists:
-        logger.info("> {}".format(l))
-
-
-# ----------------
-#  Test Functions
-# ----------------
-def test_is_palindrome():
-    """Test is_palindrome()"""
-    test_samples = ["n", "nx", "nin", "nitin", "nice"]
-    for s in test_samples:
-        r = is_palindrome(s)
-        logger.info("is_palindrom({}) = {}".format(s, r))
-
-
-def test_print_palindromic_partitions():
-    """Test print_palindromic_partitions() """
-
-    # Loop through sample strings
-    test_samples = ["nitin", "geeks", "aaracecar"]
-
-    for s in test_samples:
-        logger.info("## Checking {} ...".format(s))
-        find_palindromic_partitions(s)
 
 
 # ----------------
 #   Main
 # ----------------
-def init_logging():
-    fmt = "%(asctime)s %(levelname)s [%(funcName)s] %(message)s"
-    datefmt = '%Y-%m-%d %H:%M:%S'
-    logging.basicConfig(format=fmt, datefmt=datefmt, level=logging.DEBUG)
-
-
 def main():
     """Main function"""
-    init_logging()
-    # test_is_palindrome()
-    test_print_palindromic_partitions()
+    test_samples = ["nitin", "geeks", "aaracecar"]
+
+    sol = Solution()
+    for s in test_samples:
+        print("# Checking {} ...".format(s))
+        partitions = sol.find_palindromic_partitions(s)
+        for p in partitions:
+            print(" - {}".format(p))
 
 
 if __name__ == "__main__":
