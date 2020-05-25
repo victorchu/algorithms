86bdea8bcff1fed18968186f7db5e076f152c6fa
Victor Chu <victor@Victor-Mac.attlocal.net>
2020-05-25 16:04:48 -0700

    Updated description

--- python3/trees_and_graphs/clone_undirected_graph.py
+++ python3/trees_and_graphs/clone_undirected_graph.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
 Given a reference of a node in a connected undirected graph.
-
 Return a deep copy (clone) of the graph.
 
 Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
