65f274ad7e52dfb50e1f6a52205c1ac393597208
Victor Chu <victor@Victor-Mac.attlocal.net>
2020-07-08 15:42:00 -0700

    utility

---
+++ python3/linked_list/linked_list_utils.py
@@ -0,0 +1,33 @@
+"""
+Shared utililities for Linked Lists.
+"""
+
+from typing import List
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def lnode2str(p: ListNode) -> str:
+    """Convert a ListNode to a string."""
+    x = list()
+    while (p):
+        x.append(str(p.val))
+        p = p.next
+    return "({})".format(", ".join(x))
+
+
+def make_linked_list(vals):
+    p = prehead = ListNode()
+    for v in vals:
+        p.next = ListNode(v)
+        p = p.next
+    return prehead.next
+
+
+def make_linked_lists(lists):
+    return [make_linked_list(vals) for vals in lists]
+
+
